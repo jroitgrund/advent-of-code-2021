e9da283157accc67467e3a918420d42bb1a5c484
Jonathan Roitgrund <jonathan@it-really-tied-the-room-together.local>
2021-11-30 17:10:29 +0100

    remove test puzzle

--- src/common/all-puzzles.ts
+++ src/common/all-puzzles.ts
@@ -1,6 +1,3 @@
-import { part1, part2 } from "../puzzles/00";
 import { PuzzleSolver } from "./solver";
 
-export const allPuzzles: { [key: string]: PuzzleSolver } = {
-  "00": [part1, part2],
-};
+export const allPuzzles: { [key: string]: PuzzleSolver } = {};
--- src/puzzles/00.ts
+++
@@ -1,2 +0,0 @@
-export const part1 = (input: string) => (input === "abcdefg" ? 0 : NaN);
-export const part2 = (input: string) => (input === "abcdefg" ? 1 : NaN);
