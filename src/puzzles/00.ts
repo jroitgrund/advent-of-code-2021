43bef56c74c0c12a8c80bc0230495af7548a2189
Jonathan Roitgrund <jonathan@it-really-tied-the-room-together.local>
2021-11-30 17:19:24 +0100

    fix mistake

--- src/puzzles/00.ts
+++ src/puzzles/00.ts
@@ -1,2 +1,2 @@
-export const part1 = (input: string) => Number(input) * 2 + 1;
+export const part1 = (input: string) => Number(input) * 2;
 export const part2 = (input: string) => Number(input) ** 2;
