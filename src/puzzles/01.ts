1442e984593c29b5a6030786be6c4ea36b84856a
Romain Braun <braun.rom@gmail.com>
2021-12-01 12:31:58 +0100

    part 2

--- src/puzzles/01.ts
+++ src/puzzles/01.ts
@@ -8,3 +8,18 @@ export const puzzle1part1 = (input: string) =>
 
       0
     );
+
+export const puzzle1part2 = (input: string) =>
+  input
+    .split("\n")
+    .map(Number)
+    .reduce(
+      (previous, current, index, array) =>
+        index > 0 &&
+        array[index] + array[index + 1] + array[index + 2] >
+          array[index - 1] + array[index] + array[index + 1]
+          ? previous + 1
+          : previous,
+
+      0
+    );
