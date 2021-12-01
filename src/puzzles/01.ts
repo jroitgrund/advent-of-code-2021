b94a512ae2a13cd8f2953f7b2529c14f6e17abe5
Jonathan Roitgrund <jroitgrund@gmail.com>
2021-12-01 17:00:54 +0100

    part 2

--- src/puzzles/01.ts
+++ src/puzzles/01.ts
@@ -11,7 +11,23 @@ export function part1(input: string): number {
   return total;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function part2(_input: string): number {
-  return 0;
+export function part2(input: string): number {
+  const depths = input.split("\n").map(Number);
+
+  if (depths.length < 4) {
+    return 0;
+  }
+
+  let total = 0;
+  let window = depths[0] + depths[1] + depths[2];
+
+  for (let i = 3; i < depths.length; i++) {
+    const currWindow = window + depths[i] - depths[i - 3];
+    if (currWindow > window) {
+      total++;
+    }
+    window = currWindow;
+  }
+
+  return total;
 }
