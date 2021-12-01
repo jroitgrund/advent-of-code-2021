bab34b00352cfbb31cb4b3406716e6cff4e878a4
Romain Braun <braun.rom@gmail.com>
2021-12-01 11:26:44 +0100

    puzzle(01): untested solution

---
+++ src/puzzles/01.ts
@@ -0,0 +1,10 @@
+export const part1 = (input: string) =>
+  input
+    .split("\n")
+    .map(Number)
+    .reduce(
+      (previous, current, index, array) =>
+        current > array[index - 1] ? previous + 1 : previous,
+
+      0
+    );
