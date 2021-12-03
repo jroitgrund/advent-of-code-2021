582da7c89d6c485e969e23e128afb16f0af3a4d9
Jonathan Roitgrund <jroitgrund@gmail.com>
2021-12-03 11:58:44 +0100

    Part 3

--- src/common/all-puzzles.ts
+++ src/common/all-puzzles.ts
@@ -1,10 +1,12 @@
 import * as puzzle00 from "../puzzles/00";
 import * as puzzle01 from "../puzzles/01";
 import * as puzzle02 from "../puzzles/02";
+import * as puzzle03 from "../puzzles/03";
 import { PuzzleSolver } from "./solver";
 
 export const allPuzzles: { [key: string]: PuzzleSolver } = {
   "00": [puzzle00.part1, puzzle00.part2],
   "01": [puzzle01.part1, puzzle01.part2],
   "02": [puzzle02.part1, puzzle02.part2],
+  "03": [puzzle03.part1, puzzle03.part2],
 };
