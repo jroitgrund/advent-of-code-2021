e64bae78ee52cfdad8f9234c9f5f3f51e732fcfd
Jonathan Roitgrund <jroitgrund@gmail.com>
2021-12-04 13:49:32 +0100

    part 04

--- src/common/all-puzzles.ts
+++ src/common/all-puzzles.ts
@@ -2,6 +2,7 @@ import * as puzzle00 from "../puzzles/00";
 import * as puzzle01 from "../puzzles/01";
 import * as puzzle02 from "../puzzles/02";
 import * as puzzle03 from "../puzzles/03";
+import * as puzzle04 from "../puzzles/04";
 import { PuzzleSolver } from "./solver";
 
 export const allPuzzles: { [key: string]: PuzzleSolver } = {
@@ -9,4 +10,5 @@ export const allPuzzles: { [key: string]: PuzzleSolver } = {
   "01": [puzzle01.part1, puzzle01.part2],
   "02": [puzzle02.part1, puzzle02.part2],
   "03": [puzzle03.part1, puzzle03.part2],
+  "04": [puzzle04.part1, puzzle04.part2],
 };
---
+++ test/04.test.ts
@@ -0,0 +1,29 @@
+import { parseBoard, drawNumber, Board } from "../src/puzzles/04";
+
+test("simplest board", () => {
+  const board: Board = parseBoard("1 2 3\n4 5 6\n7 8 9");
+  expect(board.score).toBeNull();
+
+  drawNumber(board, 1);
+  expect(board.score).toBeNull();
+
+  drawNumber(board, 2);
+  expect(board.score).toBeNull();
+
+  drawNumber(board, 3);
+  expect(board.score).toBe(4 + 5 + 6 + 7 + 8 + 9);
+});
+
+test("by column", () => {
+  const board: Board = parseBoard("1 2 3\n4 5 6\n7 8 9");
+  expect(board.score).toBeNull();
+
+  drawNumber(board, 1);
+  expect(board.score).toBeNull();
+
+  drawNumber(board, 4);
+  expect(board.score).toBeNull();
+
+  drawNumber(board, 7);
+  expect(board.score).toBe(2 + 3 + 5 + 6 + 8 + 9);
+});
