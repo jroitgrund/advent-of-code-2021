36add2f5cddc35c742da9580e0ba1822075321c6
Jonathan Roitgrund <jroitgrund@gmail.com>
2021-12-06 18:14:50 +0100

    improve puzzle 06 implementation

--- src/puzzles/06.ts
+++ src/puzzles/06.ts
@@ -1,4 +1,4 @@
-import { fill, flatMap, map, sum, zipWith } from "lodash";
+import { fill, sum } from "lodash";
 
 function parseFish(input: string): FishState {
   const fish = input.match(/\d+/g)?.map(Number);
@@ -15,13 +15,18 @@ function parseFish(input: string): FishState {
 }
 
 function solution(input: string, days: number) {
-  const fish = parseFish(input);
-  const fishStatesAfterDays = fishStatesAfterNDays(days);
-  return sum(
-    flatMap(fish, (numFish, initialState) =>
-      map(fishStatesAfterDays[initialState], (n) => n * numFish)
-    )
-  );
+  let state = parseFish(input);
+  for (let day = 0; day < days; day++) {
+    const newState = Array(9) as FishState;
+    for (let i = 0; i < 8; i++) {
+      newState[i] = state[i + 1];
+    }
+    newState[8] = state[0];
+    newState[6] += state[0];
+    state = newState;
+  }
+
+  return sum(state);
 }
 
 export function part1(input: string): number {
@@ -43,46 +48,3 @@ type FishState = [
   number,
   number
 ];
-
-function fishStatesAfterNDays(days: number): FishState[] {
-  const fishStatesAfter1Day: FishState[] = [
-    [0, 0, 0, 0, 0, 0, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 0],
-  ];
-
-  let currentDayStates: FishState[] = [
-    [1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1],
-  ];
-
-  for (let day = 0; day < days; day++) {
-    const nextDayStates: FishState[] = [];
-    for (let initialCounter = 0; initialCounter <= 8; initialCounter++) {
-      nextDayStates.push(
-        zipWith(
-          ...map(currentDayStates[initialCounter], (numFish, counter) =>
-            map(fishStatesAfter1Day[counter], (n) => n * numFish)
-          ),
-          (...n) => sum(n)
-        ) as FishState
-      );
-    }
-    currentDayStates = nextDayStates;
-  }
-
-  return currentDayStates;
-}
