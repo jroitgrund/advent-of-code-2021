06cf7abfea99b709acc8e96adfe67b81e7e73ad1
Jonathan Roitgrund <jroitgrund@gmail.com>
2021-12-07 09:21:03 +0100

    part 7

--- src/common/all-puzzles.ts
+++ src/common/all-puzzles.ts
@@ -5,6 +5,7 @@ import * as puzzle03 from "../puzzles/03";
 import * as puzzle04 from "../puzzles/04";
 import * as puzzle05 from "../puzzles/05";
 import * as puzzle06 from "../puzzles/06";
+import * as puzzle07 from "../puzzles/07";
 import { PuzzleSolver } from "./solver";
 
 export const allPuzzles: { [key: string]: PuzzleSolver } = {
@@ -15,4 +16,5 @@ export const allPuzzles: { [key: string]: PuzzleSolver } = {
   "04": [puzzle04.part1, puzzle04.part2],
   "05": [puzzle05.part1, puzzle05.part2],
   "06": [puzzle06.part1, puzzle06.part2],
+  "07": [puzzle07.part1, puzzle07.part2],
 };
